8bcd4ff2912b10d380c127bd91fc8c5a08cb2a21
Daniel Sanou <daniel.sanou@yahoo.fr>
2014-01-25 15:54:19 +0100

    Classes generateur feuille et Vent

---
+++ GG2014/GG2014/Feuille.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Audio;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.GamerServices;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Media;
+namespace GG2014
+{
+    class Feuille:Object
+    {
+        public Feuille(float x, float y, Vector2 vect):base(x,y,vect)
+        { 
+        
+        }
+    }
+}
